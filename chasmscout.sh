46a492c23442b4dcb8098b16d8e69b3e5f783402
opmklinh <61728239+opmklinh@users.noreply.github.com>
2024-07-28 00:47:01 +0700

    Create chasmscout.sh

---
+++ chasmscout.sh
@@ -0,0 +1,20 @@
+PORT=3001
+LOGGER_LEVEL=debug
+
+# Chasm
+ORCHESTRATOR_URL=https://orchestrator.chasm.net
+SCOUT_NAME=huyenkul
+SCOUT_UID=9291
+WEBHOOK_API_KEY=ckUpOTRhWVh5I2Z2NFgreilWUGhrSCFZem5aKCtYVkc=
+# Scout Webhook Url, update based on your server's IP and Port
+# e.g. http://123.123.123.123:3001/
+WEBHOOK_URL=
+
+# Chosen Provider (groq, openai)
+PROVIDERS=groq
+MODEL=gemma2-9b-it
+GROQ_API_KEY=
+
+# Optional
+OPENROUTER_API_KEY=
+OPENAI_API_KEY=
